df6be63d279b9eda159841612c837de734ca9158
Juma-h <mitchfibi@gmail.com>
2022-07-28 16:59:44 +0300

    changed doc

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,6 +1,4 @@
 #!/usr/bin/env ruby
-# This script accepts one argument and passes it to a regular expression,
-# matching method
-# This script should output: [SENDER],[RECEIVER],[FLAGS]
+# This script checks if its capital letters
 
 puts ARGV[0].scan(/[A-Z]+/).join
